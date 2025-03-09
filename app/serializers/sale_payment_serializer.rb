a36f147e5ba9a8d838c294a7865a05f69f2bda71
Sofiane <contact@sofianeg.com>
2025-03-09 11:36:42 +0100

    Adding sale_id to sale_payment

--- app/serializers/sale_payment_serializer.rb
+++ app/serializers/sale_payment_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class SalePaymentSerializer < ActiveModel::Serializer
-  attributes :id, :total_amount_cents, :total_amount_currency
+  attributes :id, :total_amount_cents, :total_amount_currency, :sale_id
   belongs_to :payment_type
 end
