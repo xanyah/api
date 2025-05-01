f2ab9d2ddd64022237fbc62e83aa3b74f3dc82e2
Sofiane <contact@sofianeg.com>
2025-05-01 13:53:01 +0000

    Adding permitted_attributes to shipping variant

--- app/policies/shipping_product_policy.rb
+++ app/policies/shipping_product_policy.rb
@@ -6,4 +6,18 @@ class ShippingProductPolicy < Presets::UserEditablePolicy
       scope.where(shipping: ShippingPolicy::Scope.new(user, Shipping).resolve)
     end
   end
+
+  def permitted_attributes_for_create
+    %i[
+      shipping_id
+      product_id
+      quantity
+    ]
+  end
+
+  def permitted_attributes_for_update
+    %i[
+      quantity
+    ]
+  end
 end
