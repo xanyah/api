f95f5badb08b2f111647851e9c6374d5d54e3db0
Sofiane <contact@sofianeg.com>
2025-11-11 13:09:24 +0000

    :wrench: Adding GoodJob Engine

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
 # require "rails/test_unit/railtie"
+require 'good_job/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -45,6 +46,9 @@ module XanyahApi
 
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Session::CookieStore
     config.middleware.use config.session_store, config.session_options
+    config.middleware.use Rack::MethodOverride
   end
 end
