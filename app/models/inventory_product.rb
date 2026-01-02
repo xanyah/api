abd3ad218cdda2b53fb982e1636994beb695d4d5
Sofiane <contact@sofianeg.com>
2026-01-02 20:49:40 +0000

    :sparkles: Adding scope

--- app/models/inventory_product.rb
+++ app/models/inventory_product.rb
@@ -5,8 +5,14 @@ class InventoryProduct < ApplicationRecord
   belongs_to :inventory
   has_one :store, through: :inventory
 
+  scope :invalid_quantity, -> { eager_load(:product).where('inventory_products.quantity != products.quantity') }
+
   validates :product_id, uniqueness: { scope: :inventory_id }
   validate :inventory_locked
+
+  def self.ransackable_scopes(_auth_object = nil)
+    %i[invalid_quantity]
+  end
 
   protected
 
