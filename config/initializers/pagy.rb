80200f74441af779cc62b5859b6ff316e62ec638
Sofiane <contact@sofianeg.com>
2026-01-02 19:41:31 +0000

    :wrench: Updating default pagination limit

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -9,7 +9,7 @@
 # You can set any pagy variable as a Pagy::DEFAULT. They can also be overridden per instance by just passing them to
 # Pagy.new|Pagy::Countless.new|Pagy::Calendar::*.new or any of the #pagy* controller methods
 # Here are the few that make more sense as DEFAULTs:
-# Pagy::DEFAULT[:limit]       = 20                    # default
+Pagy::DEFAULT[:limit] = 100 # default 20
 # Pagy::DEFAULT[:size]        = 7                     # default
 # Pagy::DEFAULT[:ends]        = true                  # default
 # Pagy::DEFAULT[:page_param]  = :page                 # default
